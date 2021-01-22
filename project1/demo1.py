5ef340621e56c14a59c212dfd64b81c2a96705ae
lyh <123@qq.com>
2021-01-22 09:25:06 +0800

    flask的项目

---
+++ project1/demo1.py
@@ -0,0 +1,13 @@
+"""
+auther: lyh
+c_date: 2021/1/22 9:21
+u_date: 2021/1/22 9:21
+reversion: 1.0
+file_name: demo1
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+if __name__ == '__main__':
+    app.run()
